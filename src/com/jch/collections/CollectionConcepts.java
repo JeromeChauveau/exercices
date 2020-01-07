3b4ae12ff67717298959de2c167c50829b5356d7
A450836 <jerome.chauveau@socgen.com>
2020-01-07 07:35:54 +0100

    Initial commit

---
+++ src/com/jch/collections/CollectionConcepts.java
@@ -0,0 +1,11 @@
+package com.jch.collections;
+
+public class CollectionConcepts {
+
+    public static void main(String[] args) {
+        Product door = new Product("Wooden door", 35);
+        Product floorPanel = new Product("Floor panel", 25);
+        Product window = new Product("Window", 10);
+
+    }
+}
